228d53f4e72538d93d5474b2c7214faa810f5bbd
amithkashyap <amithkashyaprv@gmail.com>
2024-01-21 16:18:40 +0530

    refactored App component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import { Provider } from "react-redux";
 import "./App.css";
 import Body from "./components/Body";
-import Header from "./components/Header";
 import appStore from "./utils/store/appStore";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import MainContainer from "./components/MainContainer";
+import Watch from "./pages/Watch";
+import SearchResultsContainer from "./pages/SearchResultsContainer";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+    children: [
+      {
+        path: "/watch",
+        element: <Watch />,
+      },
+      {
+        path: "/results",
+        element: <SearchResultsContainer />,
+      },
+      {
+        path: "/",
+        element: <MainContainer />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <Provider store={appStore}>
       <div>
-        <Header />
-        <Body />
+        <RouterProvider router={appRouter} />
       </div>
     </Provider>
   );
