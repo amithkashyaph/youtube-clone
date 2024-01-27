8c521b324d737943aab08186242dde4b284b32e7
amithkashyap <amithkashyaprv@gmail.com>
2024-01-27 20:41:35 +0530

    added shimmer component

---
+++ src/components/Shimmer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Shimmer = () => {
+  return (
+    <div className="flex flex-wrap justify-evenly">
+      <div className="p-2 m-2 mb-10 w-72 h-80 rounded-xl bg-slate-100 shadow-lg hover:shadow-2xl"></div>
+      <div className="p-2 m-2 w-72 h-80 rounded-xl bg-slate-100 shadow-lg hover:shadow-2xl"></div>
+      <div className="p-2 m-2 w-72 h-80 rounded-xl bg-slate-100 shadow-lg hover:shadow-2xl"></div>
+      <div className="p-2 m-2 w-72 h-80 rounded-xl bg-slate-100 shadow-lg hover:shadow-2xl"></div>
+      <div className="p-2 m-2 w-72 h-80 rounded-xl bg-slate-100 shadow-lg hover:shadow-2xl"></div>
+      <div className="p-2 m-2 w-72 h-80 rounded-xl bg-slate-100 shadow-lg hover:shadow-2xl"></div>
+      <div className="p-2 m-2 w-72 h-80 rounded-xl bg-slate-100 shadow-lg hover:shadow-2xl"></div>
+      <div className="p-2 m-2 w-72 h-80 rounded-xl bg-slate-100 shadow-lg hover:shadow-2xl"></div>
+      <div className="p-2 m-2 w-72 h-80 rounded-xl bg-slate-100 shadow-lg hover:shadow-2xl"></div>
+      <div className="p-2 m-2 w-72 h-80 rounded-xl bg-slate-100 shadow-lg hover:shadow-2xl"></div>
+    </div>
+  );
+};
+
+export default Shimmer;
