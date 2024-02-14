30bbb34aba0260d01966e1f655645e1725fcedab
amithkashyap <amithkashyaprv@gmail.com>
2024-02-14 23:35:49 +0530

    added main Body component

---
+++ src/components/Body.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+
+const Body = () => {
+  return (
+    <div>
+      <Header />
+      <div className="flex w-full">
+        <Sidebar />
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default Body;
