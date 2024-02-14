a10e7d9f5f3f0776a32bcda0872fbee3b94a7da8
amithkashyap <amithkashyaprv@gmail.com>
2024-02-14 23:36:13 +0530

    added ButtonList component

---
+++ src/components/ButtonList.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Button from "./Button";
+
+const ButtonList = () => {
+  return (
+    <div className="overflow-x-scroll pl-3 py-4 mx-2 w-full">
+      <Button name="All" />
+      <Button name="Consciousness" />
+      <Button name="Gym" />
+      <Button name="CSS" />
+      <Button name="Data Structures" />
+      <Button name="Algorithms" />
+      <Button name="ReactJS" />
+      <Button name="Javascript" />
+      <Button name="HTML" />
+      <Button name="Interviews" />
+      <Button name="Google Interviews" />
+      <Button name="Gaming" />
+      <Button name="Sports" />
+      <Button name="Songs" />
+      <Button name="Programming" />
+      <Button name="Java" />
+      <Button name="Sadhguru" />
+      <Button name="Mantras" />
+      <Button name="Shlokas" />
+      <Button name="Songs retro" />
+      <Button name="Programming with Akshay" />
+      <Button name="Babel " />
+      <Button name="System Design" />
+      <Button name="Low Level Design" />
+    </div>
+  );
+};
+
+export default ButtonList;
