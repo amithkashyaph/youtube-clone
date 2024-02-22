6e41b53bcef7fa669aaf69ac4d7837465a6fbc45
amithkashyap <amithkashyaprv@gmail.com>
2024-02-22 23:59:13 +0530

    added video container component

--- src/components/VideosContainer.jsx
+++ src/components/VideosContainer.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { POPULAR_VIDEOS_URL } from "../utils/constants";
+import VideoCard from "./VideoCard";
+import { Link } from "react-router-dom";
+import Shimmer from "./Shimmer";
 
 const VideosContainer = () => {
-  return <div>VideosContainer</div>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchPopularVideos();
+  }, []);
+
+  const fetchPopularVideos = async () => {
+    const data = await fetch(POPULAR_VIDEOS_URL);
+
+    const jsonData = await data.json();
+
+    setVideos(jsonData.items);
+  };
+  return (
+    <div className="flex flex-wrap justify-evenly">
+      {videos.length ? (
+        videos.map((v) => (
+          <Link to={`/watch?v=${v.id}`} key={v.id}>
+            <VideoCard info={v} />
+          </Link>
+        ))
+      ) : (
+        <Shimmer />
+      )}
+    </div>
+  );
 };
 
 export default VideosContainer;
